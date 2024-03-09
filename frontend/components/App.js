a4f702cb51df2265b06ec919f1e0b4e369297b0d
0Jsnipes <jared.c.snipes@gmail.com>
2024-03-08 19:20:49 -0500

    done

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -25,17 +25,7 @@ function App() {
 
     }
      fetchPhoto()
-   /* setApod(
-       {
-       
-          "date": "2024-03-08",
-          "explanation": "The Tarantula Nebula, also known as 30 Doradus, is more than a thousand light-years in diameter, a giant star forming region within nearby satellite galaxy the Large Magellanic Cloud. About 180 thousand light-years away, it's the largest, most violent star forming region known in the whole Local Group of galaxies. The cosmic arachnid sprawls across this magnificent view, an assembly of image data from large space- and ground-based telescopes. Within the Tarantula (NGC 2070), intense radiation, s...",
-          "hdurl": "https://apod.nasa.gov/apod/image/2403/Tarantula-HST-ESO-Webb-LL.jpg",
-          "media_type": "image",
-          "service_version": "v1",
-          "title": "The Tarantula Zone",
-          "url": "https://apod.nasa.gov/apod/image/2403/Tarantula-HST-ESO-Webb-SS1024.jpg"
-  }) */
+   
   }, [])
   if (!apod)  return 'Fetching photo of the day...'
   return (
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,9 +1,20 @@
 import React from "react"
+import styled from "styled-components"
+
+const StyledFigure = styled.figure`
+  max-width:60%;
+
+  figcaption{
+    font-style: italic;
+  }
+
+`
+
 export default function Figure({imageURL, date}) {
     return (
-    <figure>
+    <StyledFigure>
             <img src={imageURL}/>
-            <figcaption> Awesome pic take on {date}</figcaption>
-          </figure>
+            <figcaption> Awesome pic taken on {date}</figcaption>
+          </StyledFigure>
     )
     }
